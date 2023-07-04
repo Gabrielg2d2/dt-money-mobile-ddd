dc0085cc9249c46954c71a2a7f1d31208fcd1932
G2D2 <gabriel.g2d2@gmail.com>
2023-07-04 19:53:38 -0300

    feat: modal new transaction

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,10 +3,14 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { format } from "currency-formatter";
 import {
+  Button,
   FlatList,
+  Modal,
+  SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
@@ -26,6 +30,75 @@ export function Home() {
   const [loading, setLoading] = useState(true);
   const [cards] = useState(new FactoryCards().execute());
   const [transactions] = useState(new FactoryTransaction().execute());
+  const [showModalNewTransaction, setShowModalNewTransaction] = useState(false);
+
+  function ModalNewTransaction() {
+    return (
+      <SafeAreaView>
+        <Modal
+          animationType={"slide"}
+          transparent
+          visible={showModalNewTransaction}
+        >
+          <View style={styles.modalModal}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalText}>Cadastrar transação</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  setShowModalNewTransaction(!showModalNewTransaction);
+                }}
+                style={{
+                  marginTop: 10,
+                  padding: 10,
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 20,
+                    color: "#363F5F",
+                  }}
+                >
+                  X
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <TextInput placeholder={"Nome"} style={styles.modalInput} />
+            <TextInput
+              placeholder={"Preço"}
+              keyboardType="numeric"
+              style={styles.modalInput}
+            />
+            <View
+              style={{
+                marginTop: 10,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <TouchableOpacity style={styles.modalButton}>
+                <Text>Entrada</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.modalButton}>
+                <Text>Saída</Text>
+              </TouchableOpacity>
+            </View>
+            <TextInput placeholder={"Categoria"} style={styles.modalInput} />
+            <TouchableOpacity style={styles.modalButtonSubmit}>
+              <Text
+                style={{
+                  color: "#fff",
+                }}
+              >
+                Cadastrar
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </SafeAreaView>
+    );
+  }
 
   useEffect(() => {
     async function getTransactions() {
@@ -43,90 +116,98 @@ export function Home() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={styles.header}>
-        <View style={styles.headerContainer}>
-          <View style={styles.headerContainerLogo}>
-            <View style={styles.headerLogo}>
-              <Text style={styles.headerLogoText}>$</Text>
+    <>
+      {showModalNewTransaction && <ModalNewTransaction />}
+
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.header}>
+          <View style={styles.headerContainer}>
+            <View style={styles.headerContainerLogo}>
+              <View style={styles.headerLogo}>
+                <Text style={styles.headerLogoText}>$</Text>
+              </View>
+              <Text style={styles.headerTitle}>dt money</Text>
             </View>
-            <Text style={styles.headerTitle}>dt money</Text>
-          </View>
-          <TouchableOpacity onPress={() => {}}>
-            <Text style={styles.headerButtonNewTransaction}>
-              Nova transação
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      <ScrollView horizontal style={styles.containerCards}>
-        <View style={styles.card}>
-          <Text style={styles.cardTitle}>Entradas</Text>
-          {loading ? (
-            <Text>Carregando...</Text>
-          ) : (
-            <Text style={styles.cardAmount}>
-              {formatCurrency(cards.totalCards.totalIncomingTransactions)}
-            </Text>
-          )}
-        </View>
-        <View style={styles.card}>
-          <Text style={styles.cardTitle}>Saídas</Text>
-          <Text style={styles.cardAmount}>
-            {formatCurrency(cards.totalCards.totalOutgoingTransactions)}
-          </Text>
-        </View>
-        <View
-          style={
-            cards.totalCards.totalTransactions <= 0
-              ? { ...styles.card, ...styles.cardTotalNegative }
-              : { ...styles.card, ...styles.cardTotal }
-          }
-        >
-          <Text style={styles.cardTitleTotal}>Total</Text>
-          <Text style={styles.cardAmountTotal}>
-            {formatCurrency(cards.totalCards.totalTransactions)}
-          </Text>
-        </View>
-      </ScrollView>
-
-      {loading ? (
-        <Text style={styles.listDescription}>Carregando...</Text>
-      ) : (
-        <Text style={styles.listDescription}>Listagem:</Text>
-      )}
-
-      <FlatList
-        style={styles.listContainerTransactions}
-        data={transactions.getList}
-        renderItem={({ item }) => (
-          <View style={styles.listCardTransaction}>
-            <Text style={styles.listCardTransactionDescription}>
-              {item.name}
-            </Text>
-            <Text
-              style={
-                item.type === "withdrawn"
-                  ? styles.listCardTransactionAmountOut
-                  : styles.listCardTransactionAmountInput
-              }
+            <TouchableOpacity
+              onPress={() => {
+                setShowModalNewTransaction(!showModalNewTransaction);
+              }}
             >
-              {formatCurrency(item.amount)}
-            </Text>
-            <View style={styles.listCardTransactionCategoryAndDate}>
-              <Text style={styles.listCardTransactionCategoryAndDateText}>
-                {item.category}
+              <Text style={styles.headerButtonNewTransaction}>
+                Nova transação
               </Text>
-              <Text style={styles.listCardTransactionCategoryAndDateText}>
-                {formatDate(item.date)}
-              </Text>
-            </View>
+            </TouchableOpacity>
           </View>
+        </View>
+
+        <ScrollView horizontal style={styles.containerCards}>
+          <View style={styles.card}>
+            <Text style={styles.cardTitle}>Entradas</Text>
+            {loading ? (
+              <Text>Carregando...</Text>
+            ) : (
+              <Text style={styles.cardAmount}>
+                {formatCurrency(cards.totalCards.totalIncomingTransactions)}
+              </Text>
+            )}
+          </View>
+          <View style={styles.card}>
+            <Text style={styles.cardTitle}>Saídas</Text>
+            <Text style={styles.cardAmount}>
+              {formatCurrency(cards.totalCards.totalOutgoingTransactions)}
+            </Text>
+          </View>
+          <View
+            style={
+              cards.totalCards.totalTransactions <= 0
+                ? { ...styles.card, ...styles.cardTotalNegative }
+                : { ...styles.card, ...styles.cardTotal }
+            }
+          >
+            <Text style={styles.cardTitleTotal}>Total</Text>
+            <Text style={styles.cardAmountTotal}>
+              {formatCurrency(cards.totalCards.totalTransactions)}
+            </Text>
+          </View>
+        </ScrollView>
+
+        {loading ? (
+          <Text style={styles.listDescription}>Carregando...</Text>
+        ) : (
+          <Text style={styles.listDescription}>Listagem:</Text>
         )}
-      />
-    </View>
+
+        <FlatList
+          style={styles.listContainerTransactions}
+          data={transactions.getList}
+          renderItem={({ item }) => (
+            <View style={styles.listCardTransaction}>
+              <Text style={styles.listCardTransactionDescription}>
+                {item.name}
+              </Text>
+              <Text
+                style={
+                  item.type === "withdrawn"
+                    ? styles.listCardTransactionAmountOut
+                    : styles.listCardTransactionAmountInput
+                }
+              >
+                {formatCurrency(item.amount)}
+              </Text>
+              <View style={styles.listCardTransactionCategoryAndDate}>
+                <Text style={styles.listCardTransactionCategoryAndDateText}>
+                  {item.category}
+                </Text>
+                <Text style={styles.listCardTransactionCategoryAndDateText}>
+                  {formatDate(item.date)}
+                </Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+    </>
   );
 }
 
@@ -266,5 +347,59 @@ const styles = StyleSheet.create({
   listCardTransactionCategoryAndDateText: {
     fontSize: 16,
     color: "#969CB2",
+  },
+  modalModal: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+  },
+  modalText: {
+    marginTop: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#363F5F",
+  },
+  modalHeader: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  modalInput: {
+    marginTop: 10,
+    width: "100%",
+    height: 50,
+    borderRadius: 5,
+    backgroundColor: "#E7E9EE",
+    padding: 10,
+    fontSize: 16,
+    color: "##969CB2",
+    borderColor: "#D7D7D7",
+    borderBottomWidth: 1,
+    opacity: 0.7,
+  },
+  modalButton: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#D7D7D7",
+    width: "48%",
+    display: "flex",
+    alignItems: "center",
+  },
+  modalButtonSubmit: {
+    backgroundColor: "#33CC95",
+    padding: 10,
+    borderRadius: 5,
+    width: "100%",
+    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
   },
 });
